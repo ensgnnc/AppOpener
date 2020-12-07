05a3ccbb8d2239735bde604ba7b345231dba0569
Deniz <dengun2002@gmail.com>
2020-12-07 18:05:25 +0300

    Add project files.

---
+++ AppOpener/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace AppOpener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Youtube = new System.Windows.Forms.PictureBox();
+            this.Close = new System.Windows.Forms.PictureBox();
+            this.Steam = new System.Windows.Forms.PictureBox();
+            this.LeagueOfLegends = new System.Windows.Forms.PictureBox();
+            this.Twitter = new System.Windows.Forms.PictureBox();
+            this.Spotify = new System.Windows.Forms.PictureBox();
+            this.Mail = new System.Windows.Forms.PictureBox();
+            this.Whatsapp = new System.Windows.Forms.PictureBox();
+            this.Opera = new System.Windows.Forms.PictureBox();
+            this.EpicGames = new System.Windows.Forms.PictureBox();
+            this.VisualStudio = new System.Windows.Forms.PictureBox();
+            this.OneDrive = new System.Windows.Forms.PictureBox();
+            this.Discord = new System.Windows.Forms.PictureBox();
+            this.Valorant = new System.Windows.Forms.PictureBox();
+            this.Minecraft = new System.Windows.Forms.PictureBox();
+            this.Cmd = new System.Windows.Forms.PictureBox();
+            this.Twitch = new System.Windows.Forms.PictureBox();
+            this.Logitech = new System.Windows.Forms.PictureBox();
+            this.Calculator = new System.Windows.Forms.PictureBox();
+            this.Minimize = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Youtube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Steam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeagueOfLegends)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Twitter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spotify)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Whatsapp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EpicGames)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VisualStudio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OneDrive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Discord)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Valorant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minecraft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cmd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Twitch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logitech)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Calculator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Youtube
+            // 
+            resources.ApplyResources(this.Youtube, "Youtube");
+            this.Youtube.Image = global::AppOpener.Properties.Resources.icons8_play_button_72;
+            this.Youtube.Name = "Youtube";
+            this.Youtube.TabStop = false;
+            this.Youtube.Click += new System.EventHandler(this.Youtube_Click);
+            // 
+            // Close
+            // 
+            resources.ApplyResources(this.Close, "Close");
+            this.Close.Image = global::AppOpener.Properties.Resources.icons8_cancel_72;
+            this.Close.Name = "Close";
+            this.Close.TabStop = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Steam
+            // 
+            resources.ApplyResources(this.Steam, "Steam");
+            this.Steam.Image = global::AppOpener.Properties.Resources.icons8_steam_72;
+            this.Steam.Name = "Steam";
+            this.Steam.TabStop = false;
+            this.Steam.Click += new System.EventHandler(this.Steam_Click);
+            // 
+            // LeagueOfLegends
+            // 
+            resources.ApplyResources(this.LeagueOfLegends, "LeagueOfLegends");
+            this.LeagueOfLegends.Image = global::AppOpener.Properties.Resources.icons8_league_of_legends_72;
+            this.LeagueOfLegends.Name = "LeagueOfLegends";
+            this.LeagueOfLegends.TabStop = false;
+            this.LeagueOfLegends.Click += new System.EventHandler(this.LeagueOfLegends_Click);
+            // 
+            // Twitter
+            // 
+            resources.ApplyResources(this.Twitter, "Twitter");
+            this.Twitter.Image = global::AppOpener.Properties.Resources.icons8_twitter_72;
+            this.Twitter.Name = "Twitter";
+            this.Twitter.TabStop = false;
+            this.Twitter.Click += new System.EventHandler(this.Twitter_Click);
+            // 
+            // Spotify
+            // 
+            resources.ApplyResources(this.Spotify, "Spotify");
+            this.Spotify.Image = global::AppOpener.Properties.Resources.icons8_spotify_72;
+            this.Spotify.Name = "Spotify";
+            this.Spotify.TabStop = false;
+            this.Spotify.Click += new System.EventHandler(this.Spotify_Click);
+            // 
+            // Mail
+            // 
+            resources.ApplyResources(this.Mail, "Mail");
+            this.Mail.Image = global::AppOpener.Properties.Resources.icons8_gmail_72;
+            this.Mail.Name = "Mail";
+            this.Mail.TabStop = false;
+            this.Mail.Click += new System.EventHandler(this.Mail_Click);
+            // 
+            // Whatsapp
+            // 
+            resources.ApplyResources(this.Whatsapp, "Whatsapp");
+            this.Whatsapp.Image = global::AppOpener.Properties.Resources.icons8_whatsapp_72;
+            this.Whatsapp.Name = "Whatsapp";
+            this.Whatsapp.TabStop = false;
+            this.Whatsapp.Click += new System.EventHandler(this.Whatsapp_Click);
+            // 
+            // Opera
+            // 
+            resources.ApplyResources(this.Opera, "Opera");
+            this.Opera.Image = global::AppOpener.Properties.Resources.icons8_opera_gx_72;
+            this.Opera.Name = "Opera";
+            this.Opera.TabStop = false;
+            this.Opera.Click += new System.EventHandler(this.Opera_Click);
+            // 
+            // EpicGames
+            // 
+            resources.ApplyResources(this.EpicGames, "EpicGames");
+            this.EpicGames.Image = global::AppOpener.Properties.Resources.icons8_epic_games_72;
+            this.EpicGames.Name = "EpicGames";
+            this.EpicGames.TabStop = false;
+            this.EpicGames.Click += new System.EventHandler(this.EpicGames_Click);
+            // 
+            // VisualStudio
+            // 
+            resources.ApplyResources(this.VisualStudio, "VisualStudio");
+            this.VisualStudio.Image = global::AppOpener.Properties.Resources.icons8_visual_studio_2019_72;
+            this.VisualStudio.Name = "VisualStudio";
+            this.VisualStudio.TabStop = false;
+            this.VisualStudio.Click += new System.EventHandler(this.VisualStudio_Click);
+            // 
+            // OneDrive
+            // 
+            resources.ApplyResources(this.OneDrive, "OneDrive");
+            this.OneDrive.Image = global::AppOpener.Properties.Resources.icons8_onedrive_72;
+            this.OneDrive.Name = "OneDrive";
+            this.OneDrive.TabStop = false;
+            this.OneDrive.Click += new System.EventHandler(this.OneDrive_Click);
+            // 
+            // Discord
+            // 
+            resources.ApplyResources(this.Discord, "Discord");
+            this.Discord.Image = global::AppOpener.Properties.Resources.icons8_discord_72;
+            this.Discord.Name = "Discord";
+            this.Discord.TabStop = false;
+            this.Discord.Click += new System.EventHandler(this.Discord_Click);
+            // 
+            // Valorant
+            // 
+            resources.ApplyResources(this.Valorant, "Valorant");
+            this.Valorant.Image = global::AppOpener.Properties.Resources.icons8_valorant_72;
+            this.Valorant.Name = "Valorant";
+            this.Valorant.TabStop = false;
+            this.Valorant.Click += new System.EventHandler(this.Valorant_Click);
+            // 
+            // Minecraft
+            // 
+            resources.ApplyResources(this.Minecraft, "Minecraft");
+            this.Minecraft.Image = global::AppOpener.Properties.Resources.icons8_minecraft_logo_72;
+            this.Minecraft.Name = "Minecraft";
+            this.Minecraft.TabStop = false;
+            this.Minecraft.Click += new System.EventHandler(this.Minecraft_Click);
+            // 
+            // Cmd
+            // 
+            resources.ApplyResources(this.Cmd, "Cmd");
+            this.Cmd.Image = global::AppOpener.Properties.Resources.icons8_console_72;
+            this.Cmd.Name = "Cmd";
+            this.Cmd.TabStop = false;
+            this.Cmd.Click += new System.EventHandler(this.Cmd_Click);
+            // 
+            // Twitch
+            // 
+            resources.ApplyResources(this.Twitch, "Twitch");
+            this.Twitch.Image = global::AppOpener.Properties.Resources.icons8_twitch_72;
+            this.Twitch.Name = "Twitch";
+            this.Twitch.TabStop = false;
+            this.Twitch.Click += new System.EventHandler(this.Twitch_Click);
+            // 
+            // Logitech
+            // 
+            resources.ApplyResources(this.Logitech, "Logitech");
+            this.Logitech.Image = global::AppOpener.Properties.Resources.icons8_logitech_72;
+            this.Logitech.Name = "Logitech";
+            this.Logitech.TabStop = false;
+            this.Logitech.Click += new System.EventHandler(this.Logitech_Click);
+            // 
+            // Calculator
+            // 
+            resources.ApplyResources(this.Calculator, "Calculator");
+            this.Calculator.Image = global::AppOpener.Properties.Resources.icons8_calculator_72;
+            this.Calculator.Name = "Calculator";
+            this.Calculator.TabStop = false;
+            this.Calculator.Click += new System.EventHandler(this.Calculator_Click);
+            // 
+            // Minimize
+            // 
+            resources.ApplyResources(this.Minimize, "Minimize");
+            this.Minimize.Image = global::AppOpener.Properties.Resources.icons8_minimize_window_72;
+            this.Minimize.Name = "Minimize";
+            this.Minimize.TabStop = false;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.Youtube);
+            this.Controls.Add(this.Minimize);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Steam);
+            this.Controls.Add(this.LeagueOfLegends);
+            this.Controls.Add(this.Twitter);
+            this.Controls.Add(this.Spotify);
+            this.Controls.Add(this.Mail);
+            this.Controls.Add(this.Whatsapp);
+            this.Controls.Add(this.Opera);
+            this.Controls.Add(this.EpicGames);
+            this.Controls.Add(this.VisualStudio);
+            this.Controls.Add(this.OneDrive);
+            this.Controls.Add(this.Discord);
+            this.Controls.Add(this.Valorant);
+            this.Controls.Add(this.Minecraft);
+            this.Controls.Add(this.Cmd);
+            this.Controls.Add(this.Twitch);
+            this.Controls.Add(this.Logitech);
+            this.Controls.Add(this.Calculator);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Youtube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Steam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeagueOfLegends)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Twitter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spotify)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Whatsapp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EpicGames)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VisualStudio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OneDrive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Discord)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Valorant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minecraft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cmd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Twitch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logitech)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Calculator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Calculator;
+        private System.Windows.Forms.PictureBox Logitech;
+        private System.Windows.Forms.PictureBox Twitch;
+        private System.Windows.Forms.PictureBox Valorant;
+        private System.Windows.Forms.PictureBox Minecraft;
+        private System.Windows.Forms.PictureBox Cmd;
+        private System.Windows.Forms.PictureBox VisualStudio;
+        private System.Windows.Forms.PictureBox OneDrive;
+        private System.Windows.Forms.PictureBox Discord;
+        private System.Windows.Forms.PictureBox Steam;
+        private System.Windows.Forms.PictureBox LeagueOfLegends;
+        private System.Windows.Forms.PictureBox Twitter;
+        private System.Windows.Forms.PictureBox Spotify;
+        private System.Windows.Forms.PictureBox Mail;
+        private System.Windows.Forms.PictureBox Whatsapp;
+        private System.Windows.Forms.PictureBox Opera;
+        private System.Windows.Forms.PictureBox EpicGames;
+        private System.Windows.Forms.PictureBox Close;
+        private System.Windows.Forms.PictureBox Youtube;
+        private System.Windows.Forms.PictureBox Minimize;
+    }
+}
+
---
+++ AppOpener/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppOpener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ///
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        ///
+
+
+        public static void OpenApp(string apppath)
+        {
+            System.Diagnostics.Process.Start(apppath);
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void Calculator_Click(object sender, EventArgs e)
+        {
+            OpenApp("calc.exe");
+        }
+
+        private void Cmd_Click(object sender, EventArgs e)
+        {
+            OpenApp("cmd.exe");
+        }
+
+        private void Discord_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Users/dengu/AppData/Local/Discord/app-0.0.308/Discord.exe");
+        }
+
+        private void EpicGames_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe");
+        }
+
+        private void Mail_Click(object sender, EventArgs e)
+        {
+            OpenApp("https://mail.google.com");
+            OpenApp("https://outlook.office365.com/mail/inbox");
+        }
+
+        private void LeagueOfLegends_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Riot Games/League of Legends/LeagueClient.exe");
+        }
+
+        private void Logitech_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files/Logitech Gaming Software/LCore.exe");
+        }
+
+        private void Minecraft_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files (x86)/Minecraft Launcher/MinecraftLauncher.exe");
+        }
+
+        private void OneDrive_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Users/dengu/AppData/Local/Microsoft/OneDrive/OneDrive.exe");
+        }
+
+        private void Opera_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Users/dengu/AppData/Local/Programs/Opera GX/launcher.exe");
+        }
+
+        private void Spotify_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files/WindowsApps/SpotifyAB.SpotifyMusic_1.147.684.0_x86__zpdnekdrzrea0/Spotify.exe");
+        }
+
+        private void Steam_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files (x86)/Steam/steam.exe");
+        }
+
+        private void Youtube_Click(object sender, EventArgs e)
+        {
+            OpenApp("https://www.youtube.com");
+        }
+
+        private void Twitch_Click(object sender, EventArgs e)
+        {
+            OpenApp("https://www.twitch.tv");
+        }
+
+        private void Valorant_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C \"C:/Riot Games/Riot Client/RiotClientServices.exe\" --launch-product=valorant --launch-patchline=live";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void VisualStudio_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE/devenv.exe");
+        }
+
+        private void Whatsapp_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Users/dengu/AppData/Local/WhatsApp/WhatsApp.exe");
+        }
+
+        private void Twitter_Click(object sender, EventArgs e)
+        {
+            OpenApp("https://twitter.com/home");
+        }
+
+        private void Zoom_Click(object sender, EventArgs e)
+        {
+            OpenApp("C:/Users/dengu/AppData/Roaming/Zoom/bin/Zoom.exe");
+        }
+    }
+}
